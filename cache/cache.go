1dbf133675df5b8408979d7e0eb8f7f0cbe18a6c
Julia Iskierka <jul.iskierka@gmail.com>
2024-01-18 17:14:32 +0100

    Fix invalid expired logic

--- cache/cache.go
+++ cache/cache.go
@@ -10,7 +10,7 @@ const (
 )
 
 // Possibly [DataRequest] should be part of the [cache] struct instead of the [Get] function
-// This would cause less repetition, but would mean every database request would have a separate cache instance
+// This would cause less repetition, but would mean every data request would have a separate cache instance
 // It depends on the requirements but wouldn't require big changes so I leave it this way for now
 type DataRequest func(string) interface{}
 
@@ -21,20 +21,19 @@ type Cache interface {
 	Clear()
 }
 
-type cachedData struct {
-	data      interface{}
-	expiresAt time.Time
-}
-
 type cache struct {
+	// Depending on the requirements this could be moved to the [Set]/[Get] methods
 	expiresAfter time.Duration
 	items        map[string]cachedData
 	mu           sync.Mutex
 }
 
-func New(
-	expiresAfter time.Duration,
-) Cache {
+type cachedData struct {
+	data      interface{}
+	expiresAt time.Time
+}
+
+func New(expiresAfter time.Duration) Cache {
 	return &cache{
 		expiresAfter: expiresAfter,
 		items:        map[string]cachedData{},
@@ -46,6 +45,8 @@ func (c *cache) Set(id string, data interface{}) {
 
 	var expiresAt time.Time
 	if c.expiresAfter == NeverExpires {
+		// I assume that the server will not function non stop for 99 years but if the requirements are different this could be
+		// improved
 		expiresAt = time.Now().AddDate(99, 0, 0)
 	} else {
 		expiresAt = time.Now().Add(c.expiresAfter)
@@ -64,19 +65,16 @@ func (c *cache) Get(id string, databaseRequest DataRequest) (interface{}, bool) 
 
 	item, found := c.items[id]
 
-	if !found {
+	if expired := item.expired(); !found || expired {
 		data := databaseRequest(id)
 		c.mu.Unlock()
+
 		c.Set(id, data)
+
 		return data, false
 	}
 
 	c.mu.Unlock()
-
-	if item.expired() {
-		c.delete(id)
-		return nil, false
-	}
 
 	return item.data, true
 }
@@ -84,18 +82,20 @@ func (c *cache) Get(id string, databaseRequest DataRequest) (interface{}, bool) 
 func (c *cache) DeleteExpired() {
 	for key, data := range c.items {
 		if data.expired() {
-			delete(c.items, key)
+			c.delete(key)
 		}
 	}
 }
 
 func (c *cache) Clear() {
+	c.mu.Lock()
 	c.items = make(map[string]cachedData)
+	c.mu.Unlock()
 }
 
-func (c *cache) delete(id string) {
+func (c *cache) delete(key string) {
 	c.mu.Lock()
-	delete(c.items, id)
+	delete(c.items, key)
 	c.mu.Unlock()
 
 }
